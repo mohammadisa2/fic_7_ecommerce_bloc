3dba7020bb2fb7131f8ee1afd4be7c80cdb88e39
@quzeyuah <quzeyuah@gmail.com>
2023-12-21 23:53:47 +0700

    Pagination Scroll Controller Fixed

--- lib/module/dashboard/controller/dashboard_controller.dart
+++ lib/module/dashboard/controller/dashboard_controller.dart
@@ -8,17 +8,28 @@ import '../view/dashboard_view.dart';
 class DashboardController extends State<DashboardView> {
   static late DashboardController instance;
   late DashboardView view;
+  late ScrollController scrollController;
 
   @override
   void initState() {
     instance = this;
+    scrollController = ScrollController();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        context.read<ProductsBloc>().add(const ProductsEvent.loadNextPage());
+      }
+    });
     context.read<CategoriesBloc>().add(const CategoriesEvent.getCategories());
     context.read<ProductsBloc>().add(const ProductsEvent.getAll());
     super.initState();
   }
 
   @override
-  void dispose() => super.dispose();
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => widget.build(context, this);
